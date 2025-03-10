8b8c745b8bf2b0420dca4dfab24c80c994c46098
Ibrohim Mubarok <befousmain@gmail.com>
2025-03-10 20:33:22 +0700

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # tracker
+
+## index.html
+```
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <script type="module" src="main.js"></script>
+</body>
+</html>
+```
+
+## contoh main.js menggunakan jscroot
+```
+import { getSystemInfo } from "./index.js"
+import { runAfterDOM } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.6/element.js";
+
+runAfterDOM(getSystemInfo);
+```
+
+## contoh main.js menggunakan document
+```
+import { getSystemInfo } from "./index.js"
+
+document.addEventListener("DOMContentLoaded", function() {
+    getSystemInfo();
+});
+```
---
+++ index.js
@@ -0,0 +1,51 @@
+function postBiasa(target_url,datajson,responseFunction){
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    var raw = JSON.stringify(datajson);
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch(target_url, requestOptions)
+        .then(response => {
+            const status = response.status;
+            return response.text().then(result => {
+                const parsedResult = JSON.parse(result);
+                responseFunction({ status, data: parsedResult });
+            });
+        })
+        .catch(error => console.log('error', error));
+}
+
+
+export function getSystemInfo() {
+    fetch("https://api.ipify.org?format=json")
+    .then(response => response.json())
+    .then(data => {
+        let ip = data.ip;
+        let hostname = window.location.hostname;
+
+        let datajson = {
+            "ipv4": ip,
+            "hostname": hostname,
+            "browser": navigator.userAgent
+        }
+        console.log(datajson);
+        postBiasa("https://asia-southeast2-awangga.cloudfunctions.net/domyid/api/tracker", datajson, responseFunction)
+    })
+    .catch(error => {
+        console.error("Error mengambil ip dari ipify:", error);
+    });
+}
+
+function responseFunction(result) {
+    if (result.status == 200) {
+        console.log("Berhasil")
+    } else {
+        console.log("Error")
+    }
+}
